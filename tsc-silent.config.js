1dcae5d13f07bc3d8513837beeeea5a51822693d
Nicolas Caligiuri <nicolas@caligi.com>
2023-01-03 17:28:31 +0100

    no message

---
+++ tsc-silent.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    suppress: [
+        {
+            pathRegExp: '/org/mariuszgromada/math/mxparser',
+            codes: [2322, 2339, 2341, 2345, 2445, 2531, 2540, 2565, 7006, 7010, 7023, 7053, 18046],
+        },
+    ],
+};
